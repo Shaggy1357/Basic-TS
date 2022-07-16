aaaff63c5762034176b64b7a0d383fc5b23b1ea2
pawan <pawan.bansari@creolestudios.com>
2022-07-16 14:00:39 +0530

    saving

--- new.js
+++ new.js
@@ -3,16 +3,16 @@ var obj = {name:'pawan', lastname:'bansari', age:26};
 var num = [1,2,3,4,5,6];
 
 for(i=0; i<num.length; i++){
-    console.log(i);
-    console.log(num[i]);
+    console.log(i+':'+num[i]);
+    //console.log(num[i]);
 }
 
 for(i of num){
-    console.log(i);
-    console.log(num[i]);
+    console.log(i+':'+num[i]);
+    //console.log(num[i]);
 }
 
 for(i in num){
-    console.log(i);
-    console.log(num[i]);
+    console.log(i+':'+num[i]);
+    //console.log(num[i]);
 }
