c3d6d8099ec2a8b697d9eb6cab084611503aca80
pawan <pawan.bansari@creolestudios.com>
2022-07-18 12:46:52 +0530

    changes in arrays.js

---
+++ arrays.js
@@ -0,0 +1,61 @@
+//Declaring array in single statement
+
+var num = [1,2,3,4,5,6];
+
+//Decalaring array of length 4 by using array object/constructor
+
+var num1 = new Array(4);
+
+//Adding elements to the array
+
+num1 = [7,8,9,10];
+
+// console.log('Logging array elements by using for loop');
+
+// for(var i = 0; i<num.length; i++){
+//     console.log(num[i]);
+//     console.log(i + ':' + num[i]);  // printing array elements along with indexes
+// }
+
+// for(var i = 0; i<num1.length; i++){
+//     console.log(num1[i]);
+//     console.log(i + ':' + num1[i]);
+// }
+
+// console.log('Logging array elements by using for--of loop');
+
+// for(var i = 0; i<num1.length; i++){
+//     console.log(num1[i]);
+//     console.log(`${num1[i]} is at ${i}`);
+// }
+
+// console.log('Logging array elements by using for--each loop');
+
+// function print(v,i){
+//     console.log(v);
+//     console.log(`${v} is at ${i}`);
+// } 
+
+// num.forEach(print);
+
+// console.log('Logging array elements by using for--in loop');
+
+// for(var i in num1){
+//     console.log(i);   //Gives index of the element
+//     console.log(num1[i]);  //Gives value at that index
+//     console.log(`${num1[i]} is at ${i}`);
+// }
+
+// console.log('*****Using concat function of array*******');
+
+// var numbers = num.concat(num1);
+// console.log(numbers);
+
+var num2 = [9,10,11,12,13,14,15];
+
+function big(v,i,array){
+    return v>=10;
+    console.log(v);
+}
+
+var num3 = num2.every(big);
--- new.js
+++ new.js
@@ -1,6 +1,7 @@
 var obj = {name:'pawan', lastname:'bansari', age:26};
 
 var num = [1,2,3,4,5,6];
+num.name = 'pawan';
 
 // for(i=0; i<num.length; i++){
 //     console.log(i+':'+num[i]);
@@ -9,17 +10,54 @@ var num = [1,2,3,4,5,6];
 
 // for(i of num){
 //     //console.log(i+':'+num[i]);
+//     //console.log(num[i]);
 //     console.log(i);
 // }
 
-// for(i in num){   //only takes the key as a parameter
-//     console.log(i+':'+num[i]);
-//     console.log(num[i]);
+// for(const [i, x] of num.entries()){
+//     console.log(`${x} is at ${i}`);
 // }
 
-for(i in obj){
-    console.log(`${i}:${obj[i]}`);
-    console.log(i);
-    console.log(obj[i]);
+const ratings = [
+    {user: 'John',score: 3, age:20},
+    {user: 'Jane',score: 4, age:25},
+    {user: 'David',score: 5, age:30},
+    {user: 'Peter',score: 2, age:35},
+];
+
+//let sum = 0;
+for (const {user,score,age} of ratings) {
+    //sum += score;
+    console.log(`${user} scored ${score} in age ${age}`)
 }
 
+//console.log(`Total scores: ${sum}`); // 14
+
+
+// for(i in num){   //only takes the key as a parameter
+//     //console.log(i+':'+num[i]);
+//     //console.log(num[i]);
+//     // if(i==2) break;
+//     // else{
+//         console.log(i);
+//         console.log(num[i]);
+//     //}
+// }
+
+// for(i in obj){
+//     console.log(`${i}:${obj[i]}`);
+//     console.log(i);
+//     console.log(obj[i]);
+// }
+
+// let sum = 0;
+// num.forEach(myFunction);
+// console.log(sum);
+// function myFunction(item) {
+//   sum += item;
+// }
+
+// num.forEach(func);
+// function func(item,index){
+//     console.log(index);
+// }
