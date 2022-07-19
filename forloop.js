f43b281fde75e3e6fb3c91fc8d0446805246813a
pawan <pawan.bansari@creolestudios.com>
2022-07-19 14:52:52 +0530

    saving

--- forloop.js
+++ forloop.js
@@ -3,14 +3,14 @@ let pets = ['dog', 'cat', 'lizard']
 pets[3] = 'rabbit';
 
 pets['name'] = 'pawan';
-
+console.log(pets);
 pets.sirname = 'bansari';
 
 let person = {name: "Pawan", lastname: "Bansari", age: 34}
 person.status = 'married';
 
 for(i=0 ; i<pets.length ; i++){
-    if(pets[i] == 'cat') break;
+    //if(pets[i] == 'cat') break;
     console.log(i);
     console.log(pets[i]);
 }
@@ -27,5 +27,5 @@ for(let petid in pets){
 
 for(let petid of pets){
     if(pets[petid] == 'cat') break;
-    console.log(petid);
+    console.log('-----',petid);
 }
